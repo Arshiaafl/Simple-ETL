5bcf63d0906ff69517e2ec4f2db6f769feded503
arshiaafl <arshiaaflaki@gmail.com>
2024-09-09 20:33:52 -0600

    Initial commit for ETL project

---
+++ main.py
@@ -0,0 +1,24 @@
+from pyxt.spot import Spot
+import pandas as pd
+import sqlalchemy
+
+# Fetch data from the API
+api_key = "7b5c9acc-a2f2-4859-a19c-135334ac8a33"
+secret_key = "24b148e26fd42dd4ac8df125aa88420db1338991"
+xt = Spot(host="https://sapi.xt.com", access_key=api_key, secret_key=secret_key)
+responseData = xt.get_kline(symbol='btc_usdt', interval="1d")
+
+# Convert the response to JSON and normalize it into a DataFrame
+
+df = pd.json_normalize(responseData)
+
+#df_users = pd.read_csv('users.csv', header=None, names=['user_ID', 'movie_ID', 'rating'], index_col=False)
+#df_movies = pd.read_csv('movies.csv', header=None, names=['movie_ID', 'Title', 'Description', 'Genre'], index_col=False)
+
+sqlite_file_path = 'BTC_data.db'
+
+# Create a connection to SQLite and save the data
+engine = sqlalchemy.create_engine(f'sqlite:///{sqlite_file_path}', echo=False)
+df.to_sql(name="Crypto", con=engine, index=False, if_exists='replace')
+
+
